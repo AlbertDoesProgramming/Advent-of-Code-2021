1bb9aae1f503f87b27aea83229c7ee326d151510
Albert.Evans <albertwillevans@gmail.com>
2021-12-02 18:45:34 +0000

    Day 2 Complete!

--- Day2/day2_solution.py
+++ Day2/day2_solution.py
@@ -1,6 +1,3 @@
-from os import read
-
-
 def main(input1):
     
     def readfile(textfile):
@@ -8,8 +5,41 @@ def main(input1):
         return f.read().split("\n")
     
     def getPosition(input1):
-        directions = readfile(input1)
-        return directions
+        directions = [x.split(' ') for x in readfile(input1)] 
+        x = 0
+        y = 0
+        for i in directions:
+            direction = i[0]
+            distance = int(i[1])
+            if direction == 'forward':
+                x += distance
+            elif direction == 'back':
+                x -= distance
+            elif direction == 'up':
+                y -= distance
+            elif direction == 'down':
+                y += distance
+        return x*y
+    
+    def getPositionWithAim(input1):
+        directions = [x.split(' ') for x in readfile(input1)] 
+        x = 0
+        y = 0
+        aim = 0
+        for i in directions:
+            direction = i[0]
+            command = int(i[1])
+            if direction == 'forward':
+                x += command
+                y += (command*aim)
+            elif direction == 'up':
+               aim -= command
+            elif direction == 'down':
+                aim += command
+        return x*y
+
+    print(f"Problem 1 answer is: {getPosition(input1)}")
+    print(f"Problem 1 answer is: {getPositionWithAim(input1)}")
 
 if __name__ == "__main__":
     main('Day2\day2_input2.txt')
