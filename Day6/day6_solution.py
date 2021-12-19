79a0528555ed7a6eb2049ca6acc6cc134ae5bff3
Albert.Evans <albertwillevans@gmail.com>
2021-12-19 00:31:46 +0000

    Finally Optimised

--- Day6/day6_solution.py
+++ Day6/day6_solution.py
@@ -1,9 +1,26 @@
+import numpy as np
+from numpy import bincount
+
 def main(input):
     
     def readfile(textfile):
         with open(file=f"{textfile}", mode="r") as f:
-            return f.read().strip().split('\n')
+            return f.read().strip().split(',')
 
-    
+    def getFishCountEfficiently(input: list, days: int):
+        listOfFish = np.array([int(x) for x in readfile(input)])
+        counter = np.concatenate((bincount(listOfFish), [0,0,0]))
+        for day in range(days):
+            reproducers = counter[0]
+            for i in range(8):
+                counter[i] = counter[i+1]
+            counter[8] = reproducers
+            counter[6] += reproducers
+        return sum(counter)
+
+    print(f'Problem 1 Solution With A More Efficient Function: \n {getFishCountEfficiently(input, 80)}')
+    print(f'Problem 2 Solution With A More Efficient Function: \n {getFishCountEfficiently(input, 256)}')
+
 if __name__ == "__main__":
-    main('Day5\day5_input.txt')
+    main('Day6\day6_input.txt')
+
