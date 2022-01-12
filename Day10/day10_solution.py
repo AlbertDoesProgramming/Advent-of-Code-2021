5a61dc8710d8fba31bdd3a1b585ded6f3605cd59
Albert.Evans <albertwillevans@gmail.com>
2022-01-12 22:04:56 +0000

    geting ready fr day10

---
+++ Day10/day10_solution.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def main(input):
+    
+    def readfile(textfile):
+        with open(file=f"{textfile}", mode="r") as f:
+            return f.read()
+
+    
+    
+if __name__ == "__main__":
+    main('Day10\day10_input.txt')
