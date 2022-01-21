5a43e39252aefb2c046b10ade3c8d76cdf305e20
Albert.Evans <albertwillevans@gmail.com>
2022-01-21 12:49:19 +0000

    Tidying up and adding the tasks in

---
+++ Day11/day11_solution.py
@@ -0,0 +1,11 @@
+def main(input):
+    
+    def readfile(textfile):
+        with open(file=f"{textfile}", mode="r") as f:
+            return f.read()    
+    
+    
+    
+if __name__ == "__main__":
+    main('Day11\day11_input.txt')
+    
